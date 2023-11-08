a7a109403f4d4642086cabcf9dbf720ae55f20cb
Rafid Al-Humaimidi <rafidka@gmail.com>
2023-11-08 17:52:52 +0000

    [WIP] Supporting 2.7.2

--- docker/config/webserver_config.py
+++ docker/config/webserver_config.py
@@ -25,10 +25,10 @@ from flask_appbuilder.security.manager import AUTH_DB
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = conf.get('core', 'SQL_ALCHEMY_CONN')
+SQLALCHEMY_DATABASE_URI = conf.get('database', 'SQL_ALCHEMY_CONN')
 
 # Flask-WTF flag for CSRF
-CSRF_ENABLED = True
+CSRF_ENABLED = False
 
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = False
